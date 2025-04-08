f4dd46047de3803953ef6c1b8b6edf0cd836a055
LutetiumCalcium <2501340038lee@gmail.com>
2025-04-08 09:00:25 +0900

    Add files via upload

---
+++ 5-2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{
+	int a = 200;
+
+	if (a < 100)
+	{
+		printf("Less than 100 \n");
+		printf("Can not see that less than 100 \n");
+
+	}
+	printf("The end of program \n");
+
+}
