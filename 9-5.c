d3e27f40835e597ff9280159b0fc8464dbb4a7a1
LutetiumCalcium <2501340038lee@gmail.com>
2025-04-15 08:52:33 +0900

    Add files via upload

---
+++ 9-5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main()
+{
+	int aa[3] = { 10,20,30 };
+
+	printf("&aa[0] is %d, aa+0 is %d. \n",&aa[0], aa+0);
+	printf("&aa[1] is %d, aa+1 is %d. \n", &aa[1], aa + 1);
+	printf("&aa[2] is %d, aa+2 is %d. \n", &aa[2], aa + 2);
+}
