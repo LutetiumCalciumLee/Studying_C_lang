3e1f60a0d412831b624fd7c06cf46c3af2263114
LutetiumCalcium <2501340038lee@gmail.com>
2025-05-15 09:05:14 +0900

    Add files via upload (#4)

---
+++ 03-13.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+	int a, b;
+	char c, d;
+
+	a = 0x41;
+	b = 0x50;
+
+	printf("%c \n",b);
+
+	c = a;
+	printf("%c \n",c);
+
+	d = '#';
+	printf("The ASCII value of %c is %d. \n",d,d);
+}
