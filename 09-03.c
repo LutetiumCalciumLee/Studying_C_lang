1432c93c05cd599ce4e315d08309977f2613c31b
LutetiumCalcium <2501340038lee@gmail.com>
2025-05-28 09:21:56 +0900

    Add files via upload

---
+++ 09-03.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+{
+    int a = 100;
+    int b = 200;
+
+    printf("The address of variable a is %p. \n", &a);
+    printf("The address of variable b is %p. \n", &b);
+}
